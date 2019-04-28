41ac56e6a02a2ab116823249a9c2409d2bdbc32d
Frank Whitlock <frankwhitlock@gmail.com>
2019-04-28 19:07:49 -0400

    init project created to test kotlin integration

---
+++ SwiftTest/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  SwiftTest
+//
+//  Created by Franklyn Whitlock on 4/28/19.
+//  Copyright © 2019 Franklyn Whitlock. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func showMessage(sender: UIButton) {
+        let alertController = UIAlertController(title: "Welcome to My First App", message: "Hello World", preferredStyle: UIAlertController.Style.alert)
+        alertController.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: nil))
+        present(alertController, animated: true, completion: nil)
+    }
+}
+
